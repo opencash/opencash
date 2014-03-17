3ed0ff9eac5f686a02fd4b63f2be0421a4e458e8
Serge Gebhardt <serge.gebhardt@gmail.com>
2014-03-17 14:51:48 -0300

    Travis test to build arm

--- libopencash/gradle-build.sh
+++ libopencash/gradle-build.sh
@@ -8,4 +8,22 @@ set -e
 GRADLE_FLAGS=
 [ "$TRAVIS_COMMIT" ] && GRADLE_FLAGS='--no-color -q'
 
-./gradlew $GRADLE_FLAGS test publish
+echo "./gradlew test"
+./gradlew $GRADLE_FLAGS test
+echo "after:"
+find build -type f
+
+echo "./gradlew build"
+./gradlew $GRADLE_FLAGS build
+echo "after:"
+find build -type f
+
+echo "./gradlew buildNarAndroid-armDebugOpencashStaticLibrary"
+./gradlew $GRADLE_FLAGS buildNarAndroid-armDebugOpencashStaticLibrary
+echo "after:"
+find build -type f
+
+echo "./gradlew publish"
+./gradlew $GRADLE_FLAGS publish
+echo "after:"
+find build -type f
