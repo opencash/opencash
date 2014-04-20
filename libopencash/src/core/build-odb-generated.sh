7c29dde3acfdf9e07e68c1224396a6bdd121415a
Serge Gebhardt <serge.gebhardt@gmail.com>
2014-04-20 01:55:46 -0300

    ODB class generation extracted into separate script

---
+++ libopencash/src/core/build-odb-generated.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+odb --std c++11 -d sqlite --generate-query --generate-schema -I ../../include -I ../../build/nar-dependencies/compileOpencashOsx-x86_64Debug/include --include-with-brackets --include-prefix "opencash/core" --include-regex '|opencash/core/(.+)-odb\.(.?)xx|$1-odb.$2xx|' -o generated ../../include/opencash/core/Account.h ../../include/opencash/core/Split.h ../../include/opencash/core/Transaction.h
